6100882a995ccb40a4f1ee1c67a9d88831ab5478
Pedro Scharrff <pedroscharrff@hotmail.com>
2026-05-31 14:40:05 -0400

    Revert "inicio da mudança de front"
    This reverts commit ad2860823fb32c175e41e9b3518228f451bc3ae2.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,28 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "sonner";
 import { AuthProvider } from "./contexts/AuthContext";
-import { ThemeProvider } from "./components/theme-provider";
-import { TooltipProvider } from "./components/ui/tooltip";
-import { queryClient } from "./lib/query-client";
 import { App } from "./App";
-import "./index.css";
+import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider>
-        <BrowserRouter>
-          <AuthProvider>
-            <TooltipProvider delayDuration={150}>
-              <App />
-              <Toaster richColors closeButton position="top-right" theme="system" />
-            </TooltipProvider>
-          </AuthProvider>
-        </BrowserRouter>
-      </ThemeProvider>
-    </QueryClientProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
